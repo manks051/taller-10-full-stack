ae59981ce2c4975ddc6b6cb79031279e4dea085f
manks051 <manks051@gmail.com>
2023-11-19 10:55:24 -0500

    punto 1 finalizado

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 <p>Profesor: Cristian Patiño</p>
 
 <h2>Punto 1: Constantes y Variables</h2>
+<img src="./public/images/punto-1.PNG" alt="punto 1">
 
 <h2>Punto 2: Suma de Constantes y Variables</h2>
 
---
+++ punto-1/index.php
@@ -0,0 +1,11 @@
+/// DATOS DE ENTRADA CONSTANTE Y VARIABLE
+const EXERCISE_ONE = " Bienvenido al primer punto del taller 10,";
+$name = "Andres";
+
+/// DATOS DE SALIDA
+echo EXERCISE_ONE . " " . $name . "\n";
+
+/// CAMBIO DE VARIABLE
+$name = "Mauricio";
+
+echo EXERCISE_ONE . " " . $name;
